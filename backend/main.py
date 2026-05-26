dc461d4a3bee0b83fd75670ec4acfaff5536aab2
Shana Czane M. Cruzat <shanaczanecruzat@gmail.com>
2026-05-26 11:24:28 +0800

    feat(backend): add research agent route with DuckDuckGo web search and LangGraph

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from supabase import create_client
 from langchain_groq import ChatGroq
+from langgraph.prebuilt import create_react_agent
+from langchain_community.tools import DuckDuckGoSearchRun
 from pydantic import BaseModel
 import os
 
@@ -28,6 +30,38 @@ llm = ChatGroq(
     model="llama-3.3-70b-versatile"
 )
 
+# Set up search tool
+search = DuckDuckGoSearchRun()
+tools = [search]
+
+class ResearchRequest(BaseModel):
+    question: str
+
 @app.get("/")
 def read_root():
     return {"message": "AI Research Agent API is running!"}
+
+@app.post("/research")
+async def research(request: ResearchRequest):
+
+     # Step 1: Create agent
+    agent = create_react_agent(llm, tools)
+
+    # Step 2: Run the agent
+    result = agent.invoke({
+        "messages": [("human", request.question)]
+    })
+
+    # Step 3: Get the answer
+    answer = result["messages"][-1].content
+
+    # Step 4: Save to Supabase
+    supabase.schema("project4").table("research").insert({
+        "question": request.question,
+        "answer": answer,
+    }).execute()
+
+    # Step 5: Return Answer
+    return {
+        "answer": answer
+    }
